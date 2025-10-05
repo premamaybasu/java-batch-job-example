9de86aea75cbc8abe63123976d9bc4d930884fe4
premamaybasu <premamay.basu@outlook.com>
2025-10-05 22:24:27 +0530

    Update BatchJobAppTest.java

--- src/test/java/com/expertszen/BatchJobAppTest.java
+++ src/test/java/com/expertszen/BatchJobAppTest.java
@@ -2,6 +2,7 @@ package com.expertszen;
 
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class BatchJobAppTest {
     @Test
